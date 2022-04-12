357bf817787f2643ec3d033cac07d89532dbec3c
Gabriel Guesser <gdguesser@gmail.com>
2022-04-12 22:07:37 +0100

    Movie catalogue frontend developed with Vue, consuming a api developed with Go

---
+++ src/components/AppLogin.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="mt-5">Login</h1>
+                <hr>
+                <form-tag @myevent="submitHandler" name="myform" event="myevent">
+                    <text-input 
+                        v-model="email"
+                        label="Email"
+                        type="email"
+                        name="email"
+                        required="true">
+                    </text-input>
+
+                    <text-input 
+                        v-model="password"
+                        label="Password"
+                        type="password"
+                        name="password"
+                        required="true">
+                    </text-input>
+
+                    <hr>
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </form-tag>
+            </div>
+        </div>
+    </div> 
+</template>
+
+<script>
+import TextInput from "./forms/TextInput.vue"
+import FormTag from "./forms/FormTag.vue"
+export default {
+    name: "AppLogin",
+    components: {
+        FormTag,
+        TextInput,
+    },
+    methods: {
+        submitHandler() {
+            console.log("submitHandler called")
+
+            const payload = {
+                email: this.email,
+                password: this.password
+            }
+
+            const requestOptions = {
+                method: "POST",
+                body: JSON.stringify(payload),
+            }
+
+            fetch("http://localhost:8081/users/login", requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log("error:", data.message);
+                } else {
+                    console.log(data);
+                }
+            })
+        }
+    },
+    data() {
+        return {
+            email: "",
+            password: "",
+        }
+    },
+}
+</script>
---
+++ src/components/forms/CheckInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="form-check">
+        <input class="form-check-input" :required="required" type="checkbox" :value="value" :name="name" :id="name" :checked="checked">
+        <label class="form-check-label" :for="name"> {{label}} </label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CheckInput",
+    props: ["label", "required", "name", "value", "checked"],
+}
+</script>
