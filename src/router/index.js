2ac226e040e2e0e200ebf86a96bc470868023346
Gabriel Guesser <gdguesser@gmail.com>
2022-04-21 19:22:34 +0100

    creating stub components

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppBody from "../components/AppBody.vue"
 import AppLogin from "../components/AppLogin.vue"
+import BooksList from "../components/BooksList.vue"
+import BookSingle from "../components/BookSingle.vue"
+import BooksAdmin from "../components/BooksAdmin.vue"
+import BookEdit from "../components/BookEdit.vue"
+import UsersList from "../components/UsersList.vue"
+import UserEdit from "../components/UserEdit.vue"
 
 const routes = [
     {
@@ -12,6 +18,36 @@ const routes = [
         path: "/login",
         name: "Login",
         component: AppLogin,
+    },
+    {
+        path: "/books",
+        name: "Books",
+        component: BooksList,
+    },
+    {
+        path: "/book/:bookName",
+        name: "Book",
+        component: BookSingle,
+    },
+    {
+        path: "/admin/books",
+        name: "BooksAdmin",
+        component: BooksAdmin,
+    },
+    {
+        path: "/admin/book/:bookId",
+        name: "BookEdit",
+        component: BookEdit,
+    },
+    {
+        path: "/admin/users",
+        name: "Users",
+        component: UsersList,
+    },
+    {
+        path: "/admin/user/:userId",
+        name: "UserEdit",
+        component: UserEdit,
     },
 ]
 
