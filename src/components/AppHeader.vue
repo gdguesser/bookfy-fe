69931b3e32ab8a37aec5f61514be68ae126c3a1c
Gabriel Guesser <gdguesser@gmail.com>
2022-04-21 19:40:47 +0100

    adding other routes pointing to our stub components

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -16,58 +16,110 @@
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to ="/">Home</router-link>
+            <router-link class="nav-link active" aria-current="page" to="/"
+              >Home</router-link
+            >
           </li>
+
           <li class="nav-item">
-            <router-link v-if="store.token == ''" class="nav-link" to="/login">Login</router-link>
-            <a href="javascript:void(0);" v-else class="nav-link" @click="logout">Logout</a>
-          </li> 
+            <router-link class="nav-link active" to="/books">Books</router-link>
+          </li>
+
+          <li v-if="store.token !== ''" class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Admin
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <router-link class="dropdown-item" to="/admin/users"
+                  >Manage Users</router-link
+                >
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/admin/user/0"
+                  >Add Users</router-link
+                >
+              </li>
+              <li>
+                <router-link class="dropdown-item" to="/admin/books"
+                  >Manage Books</router-link
+                >
+              </li>
+              <li>
+                <router-link
+                  class="dropdown-item"
+                  :to="{ name: 'BookEdit', params: { bookId: 0 } }"
+                  >Add Book</router-link
+                >
+              </li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <router-link v-if="store.token == ''" class="nav-link" to="/login"
+              >Login</router-link
+            >
+            <a
+              href="javascript:void(0);"
+              v-else
+              class="nav-link"
+              @click="logout"
+              >Logout</a
+            >
+          </li>
         </ul>
 
         <span class="navbar-text">
           {{ store.user.first_name ?? "" }}
         </span>
-
       </div>
     </div>
   </nav>
 </template>
 
 <script>
-import { store } from "./store.js"
-import router from "./../router/index.js"
+import { store } from "./store.js";
+import router from "./../router/index.js";
 
 export default {
   data() {
     return {
-      store
-    }
+      store,
+    };
   },
   methods: {
     logout() {
       const payload = {
         token: store.token,
-      }
+      };
 
       const requestOptions = {
         method: "POST",
         body: JSON.stringify(payload),
-      }
+      };
 
       fetch("http://localhost:8081/users/logout", requestOptions)
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.error) {
-          console.log(response.message);
-        } else {
-          store.token = "";
-          store.user = {};
-          
-          document.cookie = "_site_data=; Path=/; SameSite=strict; Secure; Expires=Thu, 01 Jan 1970 00:00:01 GMT;"
-          router.push("/login");
-        }
-      })
-    }
-  }
-}
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.error) {
+            console.log(response.message);
+          } else {
+            store.token = "";
+            store.user = {};
+
+            document.cookie =
+              "_site_data=; Path=/; SameSite=strict; Secure; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+            router.push("/login");
+          }
+        });
+    },
+  },
+};
 </script>
