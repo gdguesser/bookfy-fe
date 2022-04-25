0e8d7a864c8d7922aaaf95196e43d1bf96183816
Gabriel Guesser <gdguesser@gmail.com>
2022-04-25 21:33:54 +0100

    creating listener for emitted events

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,73 @@
 <template>
-	<Header />
-	<div>
-		<router-view/>
-	</div>
-	<Footer />
+  <Header />
+  <div>
+    <router-view @success="success" @error="error" @warning="warning" />
+  </div>
+  <Footer />
 </template>
 
 <script>
-import Header from "./components/AppHeader.vue"
-import Footer from "./components/AppFooter.vue"
-import { store } from "./components/store.js"
+import Header from "./components/AppHeader.vue";
+import Footer from "./components/AppFooter.vue";
+import { store } from "./components/store.js";
+import notie from "notie";
 
 const getCookie = (name) => {
-	return document.cookie.split("; ").reduce((r, v) => {
-		const parts = v.split("=");
-		return parts[0] === name ? decodeURIComponent(parts[1]) : r;
-	}, "")
-}
+  return document.cookie.split("; ").reduce((r, v) => {
+    const parts = v.split("=");
+    return parts[0] === name ? decodeURIComponent(parts[1]) : r;
+  }, "");
+};
 
 export default {
-	name: "App",
-	components: {
-		Header,
-		Footer
-	},
-	data() {
-		return {
-			store
-		}
-	},
-	beforeMount() {
-		//check for a cookie
-		let data = getCookie("_site_data");
+  name: "App",
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  beforeMount() {
+    //check for a cookie
+    let data = getCookie("_site_data");
 
-		if (data !== "") {
-			let cookieData = JSON.parse(data);
+    if (data !== "") {
+      let cookieData = JSON.parse(data);
 
-			//update store
-			store.token = cookieData.token.token;
-			store.user = {
-				id: cookieData.user.id,
-				first_name: cookieData.user.first_name,
-				last_name: cookieData.user.last_name,
-				email: cookieData.user.email,
-			}
-		}
-	}
-}
+      //update store
+      store.token = cookieData.token.token;
+      store.user = {
+        id: cookieData.user.id,
+        first_name: cookieData.user.first_name,
+        last_name: cookieData.user.last_name,
+        email: cookieData.user.email,
+      };
+    }
+  },
+  methods: {
+    success(msg) {
+      notie.alert({
+        type: "success",
+        text: msg,
+      });
+    },
+    error(msg) {
+      notie.alert({
+        type: "error",
+        text: msg,
+      });
+    },
+    warning() {
+      notie.alert({
+        type: "warning",
+        text: msg,
+      });
+    },
+  },
+};
 </script>
-
 <style>
-
 </style>
