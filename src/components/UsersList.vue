e07c86d7df67c557866ef8b2a5fa1d929a6f8131
Gabriel Guesser <gdguesser@gmail.com>
2022-04-26 22:59:46 +0100

    adding active column and using vue conditional to verify and set different spam

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -4,26 +4,37 @@
       <div class="col">
         <h1 class="mt-3">All Users</h1>
       </div>
-      <hr>
+      <hr />
 
       <table v-if="this.ready" class="table table-compact table-striped">
         <thead>
           <tr>
             <th>User</th>
             <th>Email</th>
+            <th>Active</th>
             <th>Status</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="u in this.users" v-bind:key="u.id">
             <td>
-              <router-link :to="`/admin/user/${u.id}`">{{u.last_name}}, {{u.first_name}}</router-link>
+              <router-link :to="`/admin/user/${u.id}`"
+                >{{ u.last_name }}, {{ u.first_name }}</router-link
+              >
             </td>
             <td>
-              {{u.email}}
+              {{ u.email }}
+            </td>
+            <td v-if="u.active == 1">
+              <span class="badge bg-success">Active</span>
+            </td>
+            <td v-else>
+              <span class="badge bg-danger">Inactive</span>
             </td>
             <td v-if="u.token.id > 0">
-              <span class="badge bg-success" @click="logUserOut(u.id)">Logged in</span>
+              <span class="badge bg-success" @click="logUserOut(u.id)"
+                >Logged in</span
+              >
             </td>
             <td v-else>
               <span class="badge bg-danger">Not logged in</span>
@@ -40,7 +51,7 @@
 <script>
 import Security from "./security.js";
 import notie from "notie";
-import {store} from "./store.js"
+import { store } from "./store.js";
 
 export default {
   data() {
@@ -81,12 +92,12 @@ export default {
         notie.confirm({
           text: "Are you sure you want to log this user out?",
           submitText: "Log out",
-          submitCallback: function() {
-            console.log("would log out user id", id)
-          }
-        })
+          submitCallback: function () {
+            console.log("would log out user id", id);
+          },
+        });
       } else {
-        this.$emit('error', "You can't log yourself out!")
+        this.$emit("error", "You can't log yourself out!");
       }
     },
   },
