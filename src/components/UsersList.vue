24ff48b2b913a4b62f4df5738d493d96baad4777
Gabriel Guesser <gdguesser@gmail.com>
2022-04-27 21:39:05 +0100

    creating the fetch call to the log-user-out backend route

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -94,6 +94,21 @@ export default {
           submitText: "Log out",
           submitCallback: function () {
             console.log("would log out user id", id);
+            fetch(process.env.VUE_APP_API_URL + "/admin/log-user-out/" + id, Security.requestOptions(""))
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.error) {
+                notie.alert({
+                  type: "error",
+                  text: data.message,
+                })
+              } else {
+                notie.alert({
+                  type: "success",
+                  text: data.message,
+                })
+              }
+            })
           },
         });
       } else {
